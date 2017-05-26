4d3d1c178dde974efa3d604ed92733ca6c6ddfbf
vikasbguru <vikasbajajs@gmail.com>
2017-05-26 19:05:13 +0530

    Create Button.java

---
+++ awsChatbot/src/main/java/com/tech/unleashed/pojo/Button.java
@@ -0,0 +1,27 @@
+package com.tech.unleashed.pojo;
+
+import java.util.HashMap;
+import java.util.Map;
+public class Button {
+	private String text;
+	private String value;
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+}
