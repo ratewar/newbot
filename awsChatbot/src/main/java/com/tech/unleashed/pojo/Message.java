5b68e583c7ba5102d890001f440318348cddf2ff
vikasbguru <vikasbajajs@gmail.com>
2017-05-26 19:06:51 +0530

    Create Message.java

---
+++ awsChatbot/src/main/java/com/tech/unleashed/pojo/Message.java
@@ -0,0 +1,36 @@
+package com.test.entity.response.format;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Message {
+
+	private String contentType;
+	private String content;
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+}
